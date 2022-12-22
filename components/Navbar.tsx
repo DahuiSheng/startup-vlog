4202c7e8e846dfbf88e5c3fbacd2c51712088657
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2022-12-22 14:19:53 +0900

    Edit navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,21 +2,25 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
+
+        // 画面上部に固定
         <div className="navbar fixed z-50 bg-white opacity-100">
 
-            {/* タイトル：　NiFT_Blog */}
+            {/* タイトル：　 */}
             <div className="flex-1">
                 <Link href={"/"} className="btn btn-ghost normal-case text-xl text-blue-900">
-                   NGC
+                   startup-vlog
                 </Link>
             </div>
-            <div className="flex-none hidden md:visible">
+
+            {/* ハンバーガーメニュー */}
+            <div className="flex-none invisible md:visible">
                 <ul className="menu menu-horizontal p-0 font-bold text-blue-900">
-                    <Link href={"/"} className="btn btn-ghost">
-                        <li>Home</li>
+                    <Link href={"/about"} className="btn btn-ghost">
+                        <li>startup-vlogとは</li>
                     </Link>
-                    <Link href={"/event"} className="btn btn-ghost">
-                        <li>イベント</li>
+                    <Link href={"/author"} className="btn btn-ghost">
+                        <li>運営者について</li>
                     </Link>
                     <Link href={"/inquiry"} className="btn btn-ghost">
                         <li>お問い合わせ</li>
@@ -24,7 +28,7 @@ const Navbar = () => {
                 </ul>
             </div>
 
-            {/* ３つの点 */}
+            {/* ハンバーガーメニュー */}
             <div className="dropdown dropdown-end flex-none md:hidden">
                 {/* 面面に現れるボタン */}
                 <label tabIndex={0} className="btn btn-ghost btn-circle">
@@ -33,11 +37,11 @@ const Navbar = () => {
                 {/* 中身 */}
                 <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 px-6 bg-base-100 shadow">
                     <div className="card-body">
-                        <Link href={"/"} className="btn btn-ghost">
-                            Home
+                        <Link href={"/about"} className="btn btn-ghost">
+                            startup-vlogとは
                         </Link>
-                        <Link href={"/event"} className="btn btn-ghost">
-                            イベント
+                        <Link href={"/author"} className="btn btn-ghost">
+                            <li>運営者について</li>
                         </Link>
                         <Link href={"/inquiry"} className="btn btn-ghost">
                             お問い合わせ
