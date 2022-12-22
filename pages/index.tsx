5df3277e268b5e48255c408d187e2217a53e1b74
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2022-12-22 23:15:48 +0900

    タグの設定

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,43 +25,57 @@ export default function Home( { blogs, tag}: Props ) {
             <div className="content bg-white col-span-2">
 
               <div className="container w-auto justify-between px-4 pt-4 pb-12">
-                {/* 記事のマッピング */}
-                {blogs.map(blog => (
-                  <div className="rounded overflow-hidden shadow-lg mb-3 bg-white" key={blog.id}>
-                    
-                    {/* 画像の表示 */}
-                    <Link href={`/blog/${blog.id}`} passHref>
-                      <img
-                        className=" w-fit"
-                        src={blog.eyecatch.url}
-                        alt="Sunset in the mountains"
-                      />
-                    </Link>
-
-                    {/* ブログのタイトル */}
-                    <div className="px-6 py-4">
+                {/* 記事の表示： */}
+                <div className='grid grid-cols1 xl:grid-cols-2'>
+                  {/* 記事のマッピング */}
+                  {blogs.map(blog => (
+                    <div className="rounded overflow-hidden shadow-lg mb-3 mx-3 bg-white" key={blog.id}>
+                      
+                      {/* 画像の表示 */}
                       <Link href={`/blog/${blog.id}`} passHref>
-                        <div className='font-bold'>{blog.title}</div>
+                        <img
+                          className=" w-fit"
+                          src={blog.eyecatch.url}
+                          alt="Sunset in the mountains"
+                        />
                       </Link>
-                    </div>
 
-                    {/* タグの表示 */}
-                    <div className="px-6 pt-4 pb-2">
-                      {blog.tag && (
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #{blog.tag.name}
-                        </span>
-                        )}
-                    </div>
+                      {/* ブログのタイトル */}
+                      <div className="px-6 py-4">
+                        <Link href={`/blog/${blog.id}`} passHref>
+                          <div className='font-bold'>{blog.title}</div>
+                        </Link>
+                      </div>
 
-                  </div>
-                ))}
+                      {/* タグの表示 */}
+                      <div className="px-6 pt-4 pb-2">
+                        {blog.tag && (
+                          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                            #{blog.tag.name}
+                          </span>
+                          )}
+                      </div>
+
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
 
             {/* ナビゲーションを表示する */}
-            <div className="Navigation bg-green-300">
-              <p>ナビゲーション</p>
+            <div className="Navigation h-5/6 pt-4">
+              <div className='text-2xl font-bold pb-3'>
+                タグ
+              </div>
+              <ul>
+                {tag.map((tag) => (
+                  <div className="rounded w-3/5 overflow-hidden h-8 shadow-lg mb-1 mx-1 px-1 bg-white" key={tag.id}>
+                    <Link href={`/category/${tag.id}`}>
+                      {tag.name}
+                    </Link>
+                  </div>
+                ))}
+              </ul>
             </div>
           </div>
       </main>
